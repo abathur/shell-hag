16fd592f9d7873177be627f78153256417261bc8
Travis A. Everett <travis.a.everett@gmail.com>
2020-04-16 20:02:01 -0500

    really should have written this already :)
    Not sure how to cleave these back out into distinct commits. Includes
    some doc fixes, backing out some nix-specific replacement @var@, and maybe
    a bugfix or two? Nonetheless, it works for a living.

--- daemon.sh
+++ daemon.sh
@@ -10,6 +10,7 @@ HAG_EXPORT_DIR="$HAG_DATA_DIR/.exported"
 HAG_NEXT_EXPORT_FILE="$HAG_EXPORT_DIR/$(uuidgen).sql"
 
 export HAG_DB="$HAG_DATA_DIR/.db.sqlite3"
+export HAG_DB_IMPORTED="$HAG_DATA_DIR/.db.imported"
 export HAG_PIPE="$HAG_DATA_DIR/.pipe"
 
 function _hag_load_schema_and_data()
@@ -17,26 +18,78 @@ function _hag_load_schema_and_data()
 	echo ".read $HAG_SCHEMA";
 	# echo ".echo on"; # for debugging SQL errors
 	for f in "$HAG_EXPORT_DIR"/*.sql; do
-	    [ -e "$f" ] && echo ".read $f"
+		if [ -e "$f" ]; then
+			echo ".read $f"
+			echo "$f" >> "$HAG_DB_IMPORTED"
+		fi
 	done
 }
 
-function _hag_start_daemon()
+function _hag_set_up_data_dir()
 {
-	mkdir -p "$HAG_DATA_DIR/.sessions" "$HAG_EXPORT_DIR"
+	mkdir -p "$HAG_DATA_DIR/.sessions" "$HAG_DATA_DIR/.nix-shell" "$HAG_EXPORT_DIR"
 	mkfifo "$HAG_DATA_DIR/.pipe"
-	@sqlite@ "$HAG_DB" \
+	{
+		echo "# caution: generated; edits not preserved!"
+		echo ".gitignore"
+		echo ".db.*"
+		echo ".nix-shell/*"
+	} > "$HAG_DATA_DIR/.gitignore"
+}
+
+function _hag_export_new_history()
+{
+	# TODO: this can produce an empty file if there are no commands to export; be cautious since there are consequences for getting it wrong, but add code to detect an empty file here (and log it), and as long as there are never false positives, start manually removing them.
+	sqlite3 "$HAG_DB" \
 	".mode insert log" \
 	".once $HAG_NEXT_EXPORT_FILE" \
 	"SELECT user,hostname,purpose,pwd,start_time,duration,pipestatus,entered_cmd,expanded_cmd,TRUE FROM log WHERE exported IS NOT TRUE order by start_time" \
 	"UPDATE log SET exported=TRUE WHERE exported IS NOT TRUE"
-	# TODO: this can produce an empty file if there are no commands to export; be cautious since there are consequences for getting it wrong, but add code to detect an empty file here (and log it), and as long as there are never false positives, start manually removing them.
-	rm "$HAG_DB"
-	@sqlite@ "$HAG_DB" < <(_hag_load_schema_and_data)
+	echo "$HAG_NEXT_EXPORT_FILE" | sort -m -o "$HAG_DB_IMPORTED" "$HAG_DB_IMPORTED" -
+}
+
+function _hag_unimported_export_files()
+{
+	# echo ".echo on"; # for debugging SQL errors
+	for f in $(comm -13 "$HAG_DB_IMPORTED" <(ls "$HAG_EXPORT_DIR/"*.sql)); do
+		if [ -e "$f" ]; then
+			local appended=true
+			echo ".read $f"
+			echo "$f" >> "$HAG_DB_IMPORTED"
+		fi
+	done
+	[[ "$appended" ]] && sort -o "$HAG_DB_IMPORTED" "$HAG_DB_IMPORTED"
+}
+
+function _hag_import_unimported_export_files()
+{
+	# TODO: not going to sweat it much for now, but this is a lot slower than it needs to be (but still under 100ms) because it calls sqlite whether there's anything to do or not.
+	# This doesn't apply to the rebuild command--that one *always* has to run to set up the schema
+	sqlite3 "$HAG_DB" < <(_hag_unimported_export_files)
+}
+
+function _hag_rebuild_database()
+{
+	rm "$HAG_DB" "$HAG_DB_IMPORTED" 2>/dev/null
+	sqlite3 "$HAG_DB" < <(_hag_load_schema_and_data)
+}
+
+function _hag_start_daemon()
+{
+	_hag_set_up_data_dir
+
+	_hag_export_new_history
+
+	if [[ -e "$HAG_DB" && -e "$HAG_DB_IMPORTED" ]]; then
+		_hag_import_unimported_export_files
+	else
+		_hag_rebuild_database
+	fi
+
 	exec "$HAG_SRC/daemon.py"
 }
 
-# It's tempting to do some of the startup work on shutdown, but I tried using an exit trap here and it doesn't seem like we get control back between when the python daeemon is forced to stop and when this file is forced to stop.
+# It's tempting to do some of the startup work on shutdown, but I tried using an exit trap here and it doesn't seem like we get control back between when the python daemon is forced to stop and when this file is forced to stop.
 # It might be possible to do it in the python, but it's probably safest all around, in either case, to do it on startup if startup is at all acceptable.
 
 
--- hag.bash
+++ hag.bash
@@ -1,8 +1,7 @@
 # shellcheck shell=bash
 if [[ -z "$SHELLSWAIN_ABOARD" ]]; then
 	# TODO: doc what this provides?
-	# shellcheck disable=SC1091
-	source "@shellswain@"
+	source shellswain.bash
 fi
 
 # local copy of this shellswain API function
@@ -13,19 +12,30 @@ shopt -s histappend
 # TODO: this is so high up because I feel like it's a bit of a utility func that doesn't really belong in hag but is here because we need a simple expressive way to set it because it's not at all obvious on a naive read unless you've memorized the components...
 function __hag_set_title()
 {
-	echo -n -e "\033]0;" "$@" "\007"
+	# echo -n -e "\033]0;" "$@" "\007"
+	printf '\e]1;%s\a' "$*"
 }
 
 # capture the starting shell, sans leading dash if present
 HAG_SHELL="${0/-/}"
 HAG_DIR=${1:-~/.config/hag}
-HAG_SESSION_DIR="$HAG_DIR/.sessions"
-# Just Apple Terminal for now. Add new terminals later? Could also make user manually add? Maybe an install step?
-HAG_SESSION_ID=$TERM_SESSION_ID;
-# ex: Apple_Terminal_7751E932-9C21-41BC-BFF1-679774179E82.state
-HAG_SESSION_FILE="$HAG_SESSION_DIR/${TERM_PROGRAM}_${HAG_SESSION_ID}.state"
-__HAG_PREV_CMD_NUM=$HISTCMD2
-HAG_PIPE="$HAG_DIR/.pipe"
+__HAG_PREV_CMD_NUM=$HISTCMD
+export HAG_PIPE="$HAG_DIR/.pipe"
+export HAG_SESSION_DIR="$HAG_DIR/.sessions"
+
+# If an outer shell passes HAG_SESSION_ID down to us accept it as-is, and do
+# *NOT* attempt to compute other values usually derived from it.
+# CAUTION: I am using HAG_SESSION_ID as shorthand for multiple variables that
+# all need to be explicitly passed for this not to blow up in your face:
+# HAG_SESSION_ID, HAG_SESSION_FILE, HAG_PURPOSE, HISTFILE
+if [[ -n "$HAG_SESSION_ID" ]]; then
+	__load_shell_history
+else
+	# Just Apple Terminal for now. Add new terminals later? Could also make user manually add? Maybe an install step?
+	HAG_SESSION_ID=$TERM_SESSION_ID;
+	# ex: Apple_Terminal_7751E932-9C21-41BC-BFF1-679774179E82.state
+	HAG_SESSION_FILE="$HAG_SESSION_DIR/${TERM_PROGRAM}_${HAG_SESSION_ID}.state"
+fi
 
 function __hag_dehydrate() {
 	if [[ -n "$HAG_PURPOSE" ]]; then
@@ -41,6 +51,7 @@ event on before_exit __hag_dehydrate
 
 __hag_load_purpose() {
 	if [ -r "$1" ]; then
+		# shellcheck disable=SC1090
 		source "$1"
 		touch "$1"
 	fi
@@ -61,9 +72,8 @@ function __hag_rehydrate() {
 
 	# If this didn't yield a purpose name, we want to force one.
 	if [ -z "$HAG_PURPOSE" ]; then
-		local purpose="unset"
 		read -rp ":( hag doesn't have a purpose; please set one: " purpose;
-		__hag_reload_or_set_purpose "$purpose"
+		__hag_reload_or_set_purpose "${purpose:-unset}"
 		__load_shell_history
 	fi
 }
@@ -82,27 +92,24 @@ function hag(){
 			__hag_reload_or_set_purpose "$2"
 			__load_shell_history
 			;;
-		import)
-			hag_import_history.bash "$HAG_PIPE" "${@:2}"
-			;;
-		tidy)
-			__hag_tidy
-			;;
 		*)
-			echo "hag has the following subcommands:"
-			printf "   %s\n      %s\n" "purpose <name>" "Set the purpose"
-			printf "   %s\n      %s\n" "import [histfile...]" "Import one or more traditional histfiles to an existing purpose (prompts for purpose)."
-			printf "   %s\n      %s\n" "tidy" "Expunge old (14d+) sessions"
+			if ! command hag "${@}"; then
+				printf "\nThe hag profile plugin adds the following subcommands:"
+				printf "   %s\n      %s\n" "purpose <name>" "Set the purpose"
+			fi
 			;;
 	esac
 }
+export hag # TODO: I'm not sure if this is necessary for subshells or if I was just trying to publish it for the preflight script... need to test
+
+
 
 function __hag_confirm_state_files() {
 	mkdir -p "$HAG_PURPOSE_DIR"
 	if [ ! -e "$HAG_PURPOSE_INIT_FILE" ]; then
 		echo "__hag_set_purpose '$HAG_PURPOSE'" >> "$HAG_PURPOSE_INIT_FILE"
 	fi
-	echo "cd '$PWD'" > $HAG_PURPOSE_PWD_FILE
+	echo "cd '$PWD'" > "$HAG_PURPOSE_PWD_FILE"
 	ln -fs "$HAG_PURPOSE_INIT_FILE" "$HAG_SESSION_FILE"
 }
 
@@ -115,6 +122,7 @@ function __hag_set_purpose() {
 	HISTFILE="$HAG_PURPOSE_DIR/$HAG_SESSION_ID.$HAG_SHELL"
 
 	if [ -r "$HAG_PURPOSE_PWD_FILE" ]; then
+		# shellcheck disable=SC1090
 		source "$HAG_PURPOSE_PWD_FILE"
 	fi
 
@@ -140,7 +148,7 @@ function __load_shell_history() {
 			# TODO: nail down path
 			# TODO: decide/settle/document the nix @replacement@ vars?
 			# TODO: hardcoded limit; this should be based on HISTSIZE
-			@sqlite@ "file:$HOME/.config/hag/.db.sqlite3?mode=ro" '.separator "\n"' ".once $tmphist" "select '#'||substr(start_time,1,length(start_time)-6), entered_cmd from log where purpose='$HAG_PURPOSE' order by start_time ASC limit 500"
+			sqlite3 "file:$HOME/.config/hag/.db.sqlite3?mode=ro" '.separator "\n"' ".once $tmphist" "select '#'||substr(start_time,1,length(start_time)-6), entered_cmd from log where purpose='$HAG_PURPOSE' order by start_time,duration ASC limit 500"
 			history -n "$tmphist"
 			((__HAG_PREV_CMD_NUM=HISTCMD-1))
 		fi
@@ -326,9 +334,96 @@ function nix_shell_run(){
 	# shellcheck disable=SC2086
 	local $1 # TODO: this technically needs IFS change to catch everything, but I guess I can also omit it when I *know* we don't have to handle spaces?
 
-	HISTFILE="$HAG_DIR/nix-shell/$(echo "$PWD" | tr / _)" command "$command_path" --keep HISTFILE "${@:3}";
+	# for ref, old format: HISTFILE="$HAG_DIR/nix-shell/$(echo "$PWD" | tr / _)" command "$command_path" --keep HISTFILE "${@:3}";
+	# TODO: this histfile path uses the old format. If you actually write to it, hag will start interpreting nix-shell as a purpose. Which brings up a tricky point. The way I've broken everything down by .config/hag/<purpose>/<id>.<command> means that whether you call these <termid|command_hash>.<nix-shell|bash>, the *normal* structural expectation is for the history to get saved under its purpose (and, therefore, to have separate per-purpose history). I see 3 outs:
+	# 1 .config/hag/.nix-shell/<hash>
+	# 2 .config/hag/<purpose>/<termid|command_hash>.<nix-shell|bash>
+	# 3 prevent it from saving a file, and synthesize one from the db (but this is back around to needing to persist the nix-shell linkage/hash in the db somewhere)
+	# Going with #1 for now; it's the simplest to implement while keeping nix-shell history that isn't isolated per purpose
+	# Addenda: I forgot about *bare* commands which will use a shell.nix to load the environment; for those we'll hash the path so that they get their own "space" as it were.
+	local to_hash="${*:3}"
+	if [[ -z "$to_hash" ]]; then
+		to_hash="${PWD}"
+	fi
+
+	# shellcheck disable=SC2034
+	read -r hash __filename < <(md5sum <<< "${to_hash}")
+	HISTFILE="$HAG_DIR/.nix-shell/$hash" command "$command_path" --keep HAG_SESSION_ID --keep HAG_SESSION_FILE --keep HAG_PURPOSE --keep HAG_PURPOSE_DIR --keep HISTFILE "${@:3}";
 	# append --keep HISTFILE to make sure it works for pure shells (though unless it can be double-invoked, it may be a bitch to parse/massage)
-	# TODO: this works, but in pure mode we lose history timestamps and probably some other related features we might actually want to drag in with us. One approach is explicitly passing it all in; another might be hacking correct settings into .bashrc if we're in a nix shell (though, admittedly, that's making installation harder...); another might be just backing off if --pure is present?
+	# TODO: this works, but in pure mode we lose:
+	# - history timestamps
+	# - the term/tab ID
+	# - the hag purpose (I'm not ceretain hag should re-run inside...)
+	# - probably some other related features we might actually want to drag in with us.
+	#
+	# One approach is explicitly passing it all in;
+	# another might be hacking correct settings into .bashrc if we're in a nix shell (though, admittedly, that's making installation harder...);
+	# another might be backing off if --pure is present?
+
+	# So there are 3 basic models:
+	# 1. don't try
+	# 2. make hag back off, except to specify the histfile/timestamp settings,
+	# 3. fully bootstrap hag to directly capture the history
+	#
+	# apart from *methods*, let's focus on goals:
+	# - make sure this doesn't overwrite the outer-history with the inner-history
+	# - have a separate, durable history space within nix-shell. but by what parameters?
+	# 	- path
+	# 	- purpose
+	# 	- termid
+	#   - actual command text? (maybe a hash or base64 of it?)
+
+	# everything I currently have collected:
+	# nix-shell --pure
+	# nix-shell --help
+	# nix-shell -K
+	# nix-shell --pure --show-trace
+	# nix-shell --version
+	# nix-shell -p heroku -p postgresql
+	# nix-shell -p wireshark
+	# nix-shell -p uwsgi
+	# nix-shell -p yq
+	# nix-shell -p yadm
+	# nix-shell -p android-file-transfer-linuux
+	# nix-shell -p android-file-transfer-linux
+	# nix-shell -p android-file-transfer
+	# nix-shell -p heroku
+	# nix-shell -p socat
+	# nix-shell -p heroku -p postgres
+	# nix-shell -p hyperfine
+	# nix-shell --pure -p heroku -p postgresql
+	# nix-shell -p litecli
+
+	# I think I've found a razor decision here:
+	# command hash is the simplest defensible (clear, nonmagic, etc.) decision
+	# - it may miss some purpose-idiomatic context, but it seems like the best all-around way to give the user a high-confidence hit of history that shows how to run the command
+	# - near/mid intent to cut active history length
+	# 	- cutting history length reduces load time, file size, and really deep scrollback searches are a bit of a misfeature (a UI/X trap!)
+	# - longer-term intent to have a history search command
+	#
+	# CAVEATS/COUNTERPOINTS:
+	# 1. hash cmd slower than pure bash underscore subst
+	# 2. The underscore method leaves some breadcrumbs as to where/what it came from (i.e., it's obviously a nix-shell from a path). I think this might be why I've kept path this long even though it's weird.
+	# 	How, in the hash method, can you cross-correlate a nix history with the command that started the shell?
+	# 		a. Save enough context (termid, purpose, etc.) to isolate the individual tab/window that created it and just use the chronology to figure out how we got there
+	# 			- could be a correlation text file, or could go in the db maybe
+	#			- limited ability to stuff this in the history file in a key-value format like I've done with others unless I use comments to hide them from history (but, making parsing them back out harder).
+	#		b. hash all of the nix-shell commands and see what matches
+	#		c. explicitly save (whether in the file system or in the database) a nix-shell histfile and the command that created it
+	#			I think the main thing I'd need is to expand the export to catch new tables?
+
+	# there's a missing element here. I'm a little embarassed about not seeing it.
+	# nix-shell is just bash. I want to save the history. And I want to have these micro-histories, but:
+	# - I probably don't need to give a flying fuck about mapping the shell to the file for history purposes. The commands should still go into the command log as bash commands (unless I give nix-shell a distinct table)
+	# - But I probably *DO* need to think about whether I can/should preserve enough retroactive information to tell that the command was run in this-or-that nix-shell.
+	#	I guess this could be a more general thought about any bash sub-shell; should it be somehow associated with the command that created it?
+	# 		I'm not sure there's a huge value to capture here, but I guess if you tracked shell-level and the termid you could notice (and even post-hoc reconstruct) when the level goes up/down as a result of a command, but you'd have to keep these in the database if you want to be able to do it post-hoc, and you'd have to generate an ID the moment you notice it to fix-up the history at the time.
+	#		In theory you could post-hoc reconstruct this in most cases via the command log with some heuristics, but they probably have to have the whole nix-shell sequence in-frame to be able to identify it.
+	#			Is this a real important thing? From here it seems like the main thing that matters is being able to see/nav the recent history while in the shell. In what othre ways might distinguishing be material?
+	#				It might be more obvious why there's a rando commando that you don't hve installed
+	#				I guess you might want to be able to query for all commands run in a nix-shell, or all commands run in a specific nix-shell command (even if it has paged out of the local histfile)
+	#					IF YOU REPRODUCE AT THIS LEVEL there are a few options available. expand the context of the existing log table so that you can have fields that identify a nix sheell or more generic subshell uniquely or by command OR have something like 2 more tables, 1 listing nix-shell-sessions (log_id, termid, purpose), and another mapping commands from the log into it like cmd_id, nix_shell_session_id
+	# * if I get all of the bash lines into the log, and the primary place they live is the log, I can probably reconstruct anything I give some shits about wrt to individual sessions with heuristics or manual tagging later...
 }
 
 function __hag_pre_cmd() {
@@ -371,7 +466,7 @@ function __hag_run_cmd() {
 
 	if [[ -n "$wrap_command" ]]; then
 		# r(ead)l(ine)wrap lets us inject history where it wasn't. It may cause problems.
-		@rlwrap@ --always-readline -H "$command_history_file" "$command_path" "${@:3}"
+		rlwrap --always-readline -H "$command_history_file" "$command_path" "${@:3}"
 	else
 		command "$2" "${@:3}"
 	fi
@@ -390,8 +485,12 @@ function __hag_post_cmd() {
 }
 
 function __hag_record_history(){
-	# When the command number is the same (at least, during normal runs, and with history configured not to record back-to-back duplicates), it means we've entered a duplicate command! So we'll ignore it.
-	# TODO: this almost inevitably misses some edge cases around first commands, history loading, etc.
+	# When the command number is the same (at least, during normal runs), it means we've entered an ignored command! So we'll ignore it. ignored commands may include:
+	# - duplicates ignored by HISTCONTROL=ignoredups
+	# - space-prefixed commands ignored by HISTCONTROL=ignorespace
+	# - anything matched by HISTIGNORE
+	#
+	# TODO: this heuristic likely misses edge cases around first commands, history loading, etc.
 	# shellcheck disable=SC2053
 	if [[ ${shellswain[command_number]} == $__HAG_PREV_CMD_NUM ]]; then
 		return 1 # TODO: more specific exit codes?
@@ -407,10 +506,34 @@ function __hag_record_history(){
 	printf '["%s","%s",%d,%d,"%s",'"r''' %s ''',r''' %s ''']\n"  "${HAG_PURPOSE}" "${PWD}" "${shellswain[start_timestamp]}" "${shellswain[duration]}" "${shellswain[pipestatus]}" "${shellswain[command]}" "$(echo ${shellswain[command]})" >> "$HAG_PIPE"
 }
 
-# TODO: bashup is leaking through, here; am I fine with that or should this be a swain API?
-event on after_command __hag_record_history
+function __hag_should_record_history()
+{
+	# TODO: bashup is leaking through, here; am I fine with that or should this be a swain API?
+	event on after_command __hag_record_history
+}
 
-__hag_rehydrate
+# TODO: Not actually in use yet; this would be used
+# by a command that declares a purpose should no-longer
+# be recorded.
+function __hag_should_not_record_history()
+{
+	# TODO: bashup is leaking through, here; am I fine with that or should this be a swain API?
+	event off after_command __hag_record_history
+}
+
+# If there's no purpose, rehydrate
+# (which will load one, or force the user to set)
+# If the purpose IS set, we've been passed in vars
+# and need to back off to avoid overwriting.
+if [[ -z "$HAG_PURPOSE" ]]; then
+	__hag_rehydrate
+fi
+
+# If this isn't set, record history like normal
+if [[ -z "$HAG_SHOULD_NOT_RECORD_HISTORY" ]]; then
+	__hag_should_record_history
+fi
+
 # TODO: I probably need a scaffold for defining all interesting versions of python based on the ones that are on the path? Or I guess I can just make the user specify which ones to wrap. That might mean, in practice, that hag knows how to track everything below, but that my user profile has the actual list of commands that triggers it to add them?
 # I guess in python's case this could also be some slightly more efficient spec format that can do just python, python(2|3), and python(2|3).(\d+)?
 # TODO: at _some_ point, _some_ portion of the statements below and the callback methods should move out of this file (on the latter, I'm not sure how 'officially' hag should support specific commands?) This might be a plugin system?
