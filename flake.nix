661d6196bfb83e806ef6c3c2de7cfda8df4545b9
Travis A. Everett <travis.a.everett@gmail.com>
2023-01-15 19:26:13 -0600

    nix: update to released shellswain :)

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       flake = false;
       follows = "shellswain/flake-compat";
     };
-    shellswain.url = "github:abathur/shellswain/flaky-breaky-heart";
+    shellswain.url = "github:abathur/shellswain/v0.1.0";
     bats-require = {
       url = "github:abathur/bats-require";
       follows = "shellswain/bats-require";
